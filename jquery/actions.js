aa5f71887cc614e6358c7e8f605a17639c871614
Andre <elij.richards22@gmail.com>
2017-11-09 00:05:15 -0500

    refactor

--- jquery/actions.js
+++ jquery/actions.js
@@ -1,28 +1,21 @@
-$(function() {
-    scrollToSection();
-    changeFolioPics();
-    waveCounter();
-    clientQuote();
-});
-
-function scrollToSection() {
-    $('.nav-item a, .hvr-sink').click(function(e) {
-        e.preventDefault();
-        var sectionID = e.currentTarget.id + "Section";
-
-        $('.nav li:nth-child(1)').removeClass('active');
-        $('.nav li:nth-child(2)').removeClass('active');
-        $('.nav-item a').removeClass('active');
-        $(this).addClass('active');
+const scrollToSection = () => {
+    $('.nav-item a, .hvr-sink').click((e) => {
+        e.preventDefault()
+        let sectionID = e.currentTarget.id + "Section"
+        $('.nav li:nth-child(1)').removeClass('active')
+        $('.nav li:nth-child(2)').removeClass('active')
+        $('.nav-item a').removeClass('active')
+        
+        $(e.target).addClass('active')
         if (sectionID == 'aboutSection') {
-            $('.hvr-sink').removeClass('active');
-            $('.nav li:nth-child(2)').addClass('active');
+            $('.hvr-sink i').removeClass('active')
+            $('.nav li:nth-child(2)').addClass('active')
         }
 
-        $('html body').animate({
-            scrollTop: $("#" + sectionID).offset().top - 60
-        }, 1000);
-    });
+        $('html, body').animate({
+            scrollTop: $(`#${sectionID}`).offset().top - 60
+        }, 1000)
+    })
 }
 
 var branding = [
@@ -48,67 +41,70 @@ var branding = [
         ["images/portfolio/green.jpg", "The Harlem Rendezvous"],
         ["images/portfolio/maxi.jpg", "Maxi Dress"],
         ["images/portfolio/washed.jpg", "With and Without"]
-    ];
-
-function showFolioPics(tabClicked) {
-    var boxNum = 1;
+    ]
+const showFolioPics = tabClicked => {
+    var boxNum = 1
     for (var i = 0; i < 4; i++) {
-        $('.portfolioGrid > div:nth-child(' + boxNum + ') > ul > li').css('background-image', 'url(' + (tabClicked)[i][0] + ')').css('transition', 'all 0.6s');
-        $('.portfolioGrid > div:nth-child(' + boxNum + ') > ul > li > div > div.caption-text > h1').html((tabClicked)[i][1]);
-        boxNum++;
+        $('.portfolioGrid > div:nth-child(' + boxNum + ') > ul > li').css('background-image', 'url(' + (tabClicked)[i][0] + ')').css('transition', 'all 0.6s')
+        $('.portfolioGrid > div:nth-child(' + boxNum + ') > ul > li > div > div.caption-text > h1').html((tabClicked)[i][1])
+        boxNum++
     }
 }
+const changeFolioPics = () => {
+    showFolioPics(branding)
 
-function changeFolioPics() {
-    showFolioPics(branding);
-
-    $('#branding').click(function(e) {
-        e.preventDefault();
-        showFolioPics(branding);
-    });
-    $('#webDesign').click(function(e) {
-        e.preventDefault();
-        showFolioPics(webDesign);
-    });
-    $('#socMedia').click(function(e) {
-        e.preventDefault();
-        showFolioPics(socMedia);
-    });
-    $('#campaign').click(function(e) {
-        e.preventDefault();
-        showFolioPics(campaign);
-    });
-};
-
-function waveCounter() {
-    $('.count').each(function() {
+    $('#branding').click((e) => {
+        e.preventDefault()
+        showFolioPics(branding)
+    })
+    $('#webDesign').click((e) => {
+        e.preventDefault()
+        showFolioPics(webDesign)
+    })
+    $('#socMedia').click((e) => {
+        e.preventDefault()
+        showFolioPics(socMedia)
+    })
+    $('#campaign').click((e) => {
+        e.preventDefault()
+        showFolioPics(campaign)
+    })
+}
+const waveCounter = () => {
+    $('.count').each(() => {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
             duration: 6500,
             easing: 'swing',
-            step: function(now) {
-                $(this).text(Math.ceil(now));
+            step: (now) => {
+                $(this).text(Math.ceil(now))
             }
-        });
-    });
+        })
+    })
+}
+const clientQuote = () => {
+    $('.zooey span').mouseover((e) => {
+        e.preventDefault()
+        $('.clientQuote').html('\"Lorem ipsum dolor sit amet, consectetur adipisicing elit!\"')
+    })
+    $('.power span').mouseover((e) => {
+        e.preventDefault()
+        $('.clientQuote').html('\"Fusce hendrerit tristique ante, eget sagittis magna commodo ut!\"')
+    })
+    $('.jackie span').mouseover((e) => {
+        e.preventDefault()
+        $('.clientQuote').html('\"Vestibulum pharetra amet porta tincidunt. Donec dictum lorem sit!\"')
+    })
+    $('.ava span').mouseover((e) => {
+        e.preventDefault()
+        $('.clientQuote').html('\"Fusce cursus molestie risus eu finibus. Quisque dictum tellus!\"')
+    })
 }
 
-function clientQuote() {
-    $('.zooey span').mouseover(function(e) {
-        e.preventDefault();
-        $('.clientQuote').html('\"Lorem ipsum dolor sit amet, consectetur adipisicing elit!\"');
-    });
-    $('.power span').mouseover(function(e) {
-        e.preventDefault();
-        $('.clientQuote').html('\"Fusce hendrerit tristique ante, eget sagittis magna commodo ut!\"');
-    });
-    $('.jackie span').mouseover(function(e) {
-        e.preventDefault();
-        $('.clientQuote').html('\"Vestibulum pharetra amet porta tincidunt. Donec dictum lorem sit!\"');
-    });
-    $('.ava span').mouseover(function(e) {
-        e.preventDefault();
-        $('.clientQuote').html('\"Fusce cursus molestie risus eu finibus. Quisque dictum tellus!\"');
-    });
+{
+    scrollToSection()
+    changeFolioPics()
+    waveCounter()
+    clientQuote()
 }
